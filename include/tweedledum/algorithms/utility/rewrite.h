ecf3ff22a4cd3dfcc755b01c5ebffddb655b5b93
Bruno Schmitt <bruno.schmitt@epfl.ch>
2020-05-20 18:47:43 +0200

    Fix `rewrite_circuit` function.
    It also remove some useless template parameters.  I'm not done with this
    cleanup yet.

--- include/tweedledum/algorithms/utility/rewrite.h
+++ include/tweedledum/algorithms/utility/rewrite.h
@@ -4,94 +4,41 @@
 *-----------------------------------------------------------------------------*/
 #pragma once
 
+#include "../../ir/Circuit.h"
 #include "shallow_duplicate.h"
 
 #include <type_traits>
 
 namespace tweedledum {
 
-/*! \brief Generic rewrite function.
+/*! \brief Generic circuit rewriting function
  *
  * The parameter ``fn`` is any callable that must have the following signature:
- * - ``bool(network_type&, gate_type const&)``
+ * - ``bool(Circuit&, Operation const&)``
  *
  * If ``fn`` returns ``true`` then it means that the gate was rewritten and
  * nothing else will be done.  In case it returns ``false``, then it means the
- * current gate must be copied to the new network.
+ * current gate must be copied to the new circuit.
  *
- * NOTE: This function requires a template parameter that cannot be inferred.
- * This is useful when rewriting into a different network format, e.g.
- * `gg_network` <-> `Netlist``
- *
- * NOTE: Gate type _must_ be the same.
- *
- * \tparam Circuit the original circuit type.
- * \tparam NewCircuit the resulting circuit type.
  * \param[in] original the original quantum circuit (__will not be modified__).
- * \param[in] fn a rewriting function with the signature: ``bool(network_type&,
- * gate_type const&)``. \param[in] num_ancillae number of ancillae to add to the
- * network. \returns a _new_ rewritten network.
+ * \param[in] fn a rewriting function.
+ * \param[in] num_ancillae number of ancillae to add to the circuit.
+ * \returns a _new_ rewritten circuit.
  */
-template<class NewCircuit, class Circuit, class RewriteFn>
-NewCircuit rewrite_network(
-    Circuit const original, RewriteFn&& fn, uint32_t num_ancillae = 0u)
-{
-	static_assert(std::is_same_v<typename Circuit::gate_type,
-	                  typename NewCircuit::gate_type>,
-	    "Gate type _must_ be the same");
-	static_assert(std::is_invocable_r_v<bool, RewriteFn, Circuit&,
-	                  typename Circuit::gate_type>,
-	    "The rewriting function signature _must_ be ``bool(network, "
-	    "gate)``");
-
-	NewCircuit result = shallow_duplicate<NewCircuit>(original);
-	for (uint32_t i = 0u; i < num_ancillae; ++i) {
-		result.add_qubit(/* is_ancilla */ true);
-	}
-	original.foreach_gate([&](auto const& node) {
-		if (!fn(result, node.gate)) {
-			result.emplace_gate(node.gate);
-		}
-	});
-	result.rewire(original.wiring_map());
-	return result;
-}
-
-/*! \brief Generic network rewriting function
- *
- * The parameter ``fn`` is any callable that must have the following signature:
- * - ``bool(network_type&, gate_type const&)``
- *
- * If ``fn`` returns ``true`` then it means that the gate was rewritten and
- * nothing else will be done.  In case it returns ``false``, then it means the
- * current gate must be copied to the new network.
- *
- * \tparam Circuit the original circuit type.
- * \tparam NewCircuit the resulting circuit type.
- * \param[in] original the original quantum circuit (__will not be modified__).
- * \param[in] fn a rewriting function with the signature: ``bool(network_type&,
- * gate_type const&)``. \param[in] num_ancillae number of ancillae to add to the
- * network. \returns a _new_ rewritten network.
- */
-template<class Circuit, class RewriteFn>
-Circuit rewrite_network(
-    Circuit const original, RewriteFn&& fn, uint32_t num_ancillae = 0u)
+template<class RewriteFn>
+Circuit rewrite_circuit(Circuit const original, RewriteFn&& fn)
 {
 	static_assert(std::is_invocable_r_v<bool, RewriteFn, Circuit&,
-	                  typename Circuit::gate_type const&>,
-	    "The rewriting function signature _must_ be ``bool(network, "
-	    "gate)``");
+	                  typename Circuit::op_type const&>,
+	    "The rewriting function signature _must_ be bool(Circuit&, Operation const&)");
 
 	Circuit result = shallow_duplicate(original);
-	for (auto i = 0u; i < num_ancillae; ++i) {
-		result.add_qubit(/* is_ancilla */ true);
-	}
-	original.foreach_gate([&](auto const& node) {
-		if (!fn(result, node.gate)) {
-			result.emplace_gate(node.gate);
+	using op_type = typename Circuit::op_type;
+	original.foreach_op([&](op_type const& op) {
+		if (!fn(result, op)) {
+			result.emplace_op(op);
 		}
 	});
-	result.rewire(original.wiring_map());
 	return result;
 }
 
--- include/tweedledum/algorithms/utility/shallow_duplicate.h
+++ include/tweedledum/algorithms/utility/shallow_duplicate.h
@@ -4,49 +4,19 @@
 *-----------------------------------------------------------------------------*/
 #pragma once
 
+#include "../../ir/Circuit.h"
 #include "../../ir/Wire.h"
 
 #include <string>
 
 namespace tweedledum {
 
-/*! \defgroup shallow_duplicate This is a hack for documentation :(
- *  \{
- */
 /*! \brief Creates a new circuit with same wires as the original.
  *
- * This function requires a template parameter that cannot be inferred.  Useful
- * when duplicating into a different circuit type.
- *
- * \tparam Circuit the original circuit type.
- * \tparam NewCircuit the resulting circuit type.
  * \param[in] original the original quantum circuit (__will not be modified__).
  * \param[in] name the name of the new circuit (default: same as the original).
  * \returns a __new__ circuit without gates.
  */
-template<class Circuit, class NewCircuit>
-NewCircuit shallow_duplicate(Circuit const original, std::string_view name = {})
-{
-	NewCircuit duplicate(original.module(), name.empty() ? original.name() : name);
-	original.foreach_wire([&](wire::Id wire, std::string_view name) {
-		if (wire.is_qubit()) {
-			duplicate.create_qubit(name, original.wire_mode(wire));
-		} else {
-			duplicate.create_cbit(name);
-		}
-	});
-	return duplicate;
-}
-
-/*! \brief Creates a new circuit with same wires as the original.
- *
- * \tparam Circuit the original circuit type.
- * \tparam NewCircuit the resulting circuit type.
- * \param[in] original the original quantum circuit (__will not be modified__).
- * \param[in] name the name of the new circuit (default: same as the original).
- * \returns a __new__ circuit without gates.
- */
-template<class Circuit>
 Circuit shallow_duplicate(Circuit const original, std::string_view name = {})
 {
 	Circuit duplicate(original.module(), name.empty() ? original.name() : name);
@@ -59,6 +29,5 @@ Circuit shallow_duplicate(Circuit const original, std::string_view name = {})
 	});
 	return duplicate;
 }
-/*! \} */
 
 } // namespace tweedledum
